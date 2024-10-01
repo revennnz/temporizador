0afb1ad9d93017c995bd677b870e268ebe814620
revennnz <aquimovies.tk@gmail.com>
2024-10-01 05:38:28 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,54 @@
-const CACHE_NAME = 'entrenamientos-cache-v1';
+const CACHE_NAME = 'entrenamientos-cache-v2';
 const urlsToCache = [
   '',
   '/',
   'manifest.json',
   'https://i.imgur.com/St2wFsJ.png', // Ícono de la app
+const CACHE_NAME = 'temporizador-cache-v1'; 
+const urlsToCache = [
+  '',
+  '/',
+  'manifest.json', // Asegúrate de que este archivo esté presente
+  'https://i.imgur.com/St2wFsJ.png'
 ];
 
-// Instalación del Service Worker
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Archivos en caché durante la instalación:', urlsToCache);
-                return cache.addAll(urlsToCache);
-            })
-    );
+// Instalación del Service Worker y almacenamiento en caché de los recursos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-// Activación del Service Worker
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        console.log('Cache eliminada:', cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+// Interceptar las solicitudes de red para devolver contenido de la caché
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Actualizar el caché cuando hay nuevos recursos
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
 
-// Interceptar las solicitudes
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Si hay respuesta en caché, devolverla
-                if (response) {
-                    return response;
-                }
-                // Si no hay respuesta en caché, realizar la solicitud a la red
-                return fetch(event.request).then((response) => {
-                    // Clonar la respuesta para poder almacenarla en caché
-                    const responseClone = response.clone();
-                    caches.open(CACHE_NAME).then((cache) => {
-                        cache.put(event.request, responseClone);
-                    });
-                    return response;
-                });
-            })
-    );
+// Notificar al cliente sobre la actualización del Service Worker
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
